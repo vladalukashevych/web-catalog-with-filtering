39fb3d2e201c189f19c48c487d981f1a981012c8
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2023-12-06 19:48:17 +0100

    Category section

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,9 +1,41 @@
 import "./Category.css"
 
 function Category() {
-  return (
-    <div>Category</div>
-  )
+    return (
+        <div>
+            <h2 className="sidebar-title">Category</h2>
+
+            <div>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="test"/>
+                    <span className="checkmark"></span>All
+                </label>
+            </div>
+            <div>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="test"/>
+                    <span className="checkmark"></span>Sneakers
+                </label>
+            </div>
+            <div>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="test"/>
+                    <span className="checkmark"></span>Flats
+                </label>
+            </div>
+            <div>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="test"/>
+                    <span className="checkmark"></span>Sandals
+                </label>
+            </div>
+            <div>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="test"/>
+                    <span className="checkmark"></span>Heels
+                </label>
+            </div>
+        </div>);
 }
 
 export default Category;
