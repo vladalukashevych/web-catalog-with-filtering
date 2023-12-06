d1ebb48192a5dbb51f60976b238dbbe18c6b96fb
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2023-12-06 19:11:19 +0100

    Recomended section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
   return (
     <>
+        <Sidebar />
         <Navigation />
-        <Products />
         <Recommended />
+        <Products />
     </>
   );
 }
--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -2,8 +2,19 @@ import "./Recommended.css"
 
 function Recommended() {
   return (
-    <div>Recommended</div>
-  )
+      <>
+        <div>
+          <h2 className="recommended-title">Recommended</h2>
+          <div className="recommended-flex">
+            <button className="btns">All Products</button>
+            <button className="btns">Nike</button>
+            <button className="btns">Adidas</button>
+            <button className="btns">Puma</button>
+            <button className="btns">Vans</button>
+            </div>
+        </div>
+      </>
+  );
 }
 
 export default Recommended;
