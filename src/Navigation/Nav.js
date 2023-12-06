f257e4a9cf78a393be4e3ce07e9329a6d62b8c28
Andrii Sakhno <andreylion.zt@gmail.com>
2023-12-06 12:21:50 +0100

    Styling the navigation section

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,9 +1,28 @@
+import {FiHeart} from "react-icons/fi"
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from "react-icons/ai"
 import "./Nav.css"
 
 function Nav() {
-  return (
-    <div>Nav</div>
-  )
+  return <nav>
+    <div className="nav-container">
+        <input 
+        type="text"
+        className="search-input"
+        placeholder="Enter your search shoes."/>
+    </div>
+
+    <div className="profile-container">
+        <a href="#">
+            <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+            <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+            <AiOutlineUserAdd className="nav-icons" />
+        </a>
+    </div>
+  </nav>
 }
 
 export default Nav;
