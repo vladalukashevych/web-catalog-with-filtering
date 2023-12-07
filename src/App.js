06ec03a4986a9a8312722c569bb3d6dd10ccf546
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2023-12-07 12:48:09 +0100

    Input filter functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import {useState} from "react";
+
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
+// Database
+import products from "./db/data";
+
 function App() {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    //---------------Input Filter---------------
+    const[query, setQuery] = useState("");
+
+    const handleInputChange = event => {
+        setQuery(event.target.value)
+    };
+
+    const filteredItems = products.filter(
+        product => product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+    );
+
+
+
   return (
     <>
         <Sidebar />
