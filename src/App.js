4fb77fc7c82e097b667e9ff51d590391308e7c42
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2023-12-07 12:49:58 +0100

    Radio filter functionality

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ function App() {
         product => product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
     );
 
-
+    //---------------Radio Filter---------------
+    const handleChange = event => {
+        setSelectedCategory(event.target.value)
+    };
 
   return (
     <>
