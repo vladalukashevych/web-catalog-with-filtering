8094c650765d0986fb1dfc3ba9e0445e140fa4ed
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2023-12-07 12:09:05 +0100

    Price section

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -2,8 +2,40 @@ import "./Price.css"
 
 function Price() {
   return (
-    <div>Price</div>
-  )
+      <div className="ml">
+        <h2 className="sidebar-title price-title">Price</h2>
+
+        <div>
+          <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span>All
+          </label>
+        </div>
+        <div>
+          <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span>0-50$
+          </label>
+        </div>
+        <div>
+          <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span>50-100$
+          </label>
+        </div>
+        <div>
+          <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span>100-150$
+          </label>
+        </div>
+        <div>
+          <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span>150-200$
+          </label>
+        </div>
+      </div>);
 }
 
 export default Price;
