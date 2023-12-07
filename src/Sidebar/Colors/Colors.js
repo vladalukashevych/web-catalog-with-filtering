791b676325f9ba244b9fc4f0a809c336a0488c5c
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2023-12-07 12:17:37 +0100

    Colors section

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -2,8 +2,34 @@ import "./Colors.css"
 
 function Colors() {
   return (
-    <div>Colors</div>
-  )
+    <div>
+      <h2 className="sidebar-title">Colors</h2>
+        <label className="sidebar-label-container color-title">
+          <input type="radio" name="test2"/>
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2"/>
+          <span className="checkmark"></span>Black
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2"/>
+          <span className="checkmark"></span>White
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2"/>
+          <span className="checkmark"></span>Blue
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2"/>
+          <span className="checkmark"></span>Red
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2"/>
+          <span className="checkmark"></span>Green
+        </label>
+    </div>
+  );
 }
 
 export default Colors;
